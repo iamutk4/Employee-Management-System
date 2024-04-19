d6295fcf7435e032e03e8f0226864c38b2018be0
iamutk4 <utkarsh_jadon@hotmail.com>
2024-04-19 17:20:34 -0400

    added frontend code

---
+++ ems-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>
+                All Rights Reserved 2024 @Employee Management System
+            </span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
